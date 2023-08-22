a71f27902bf409b6a0da02432ca33672e3dbdd2e
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-08-22 17:49:00 +0200

    add consts JS

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const cellsContainer = document.getElementById("box-target");
+const gridButton = document.getElementById("grid-button");
+const difficultySelect = document.getElementById("difficulty");
+let bombs = [];
