9b7a804b6325b619b95654ce396d32708e63ecf8
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-08-22 17:52:04 +0200

    add functions JS

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ const cellsContainer = document.getElementById("box-target");
 const gridButton = document.getElementById("grid-button");
 const difficultySelect = document.getElementById("difficulty");
 let bombs = [];
+
+// GENERAL ARRAY
+
+function generateArray(from, to, step) {
+  let whitelist = [];
+  for (let i = from; i <= to; i += step) {
+    whitelist.push(i);
+  }
+  return whitelist;
+}
